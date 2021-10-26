f4ae3e962cbbabd4d4ecd7474237111c7cc7d139
elisaulgarcia <elisaul-garcia@hotmail.com>
2021-10-26 06:43:53 -0500

    ml

---
+++ Procfile
@@ -0,0 +1,1 @@
+node rutaAlArchivoPrincipal.js
--- src/app.js
+++ src/app.js
@@ -1,15 +1,13 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-const path = require("path")
+const path = require("path");
 
-const rutas = require("./routes/home.routes")
+const rutas = require("./routes/home.routes");
 
-let port = process.env.PORT || 3500
+let port = process.env.PORT || 3500;
 
-app.use(express.static(path.join(__dirname, "./public")))
-app.use("/", rutas)
+app.use(express.static(path.join(__dirname, "./public")));
+app.use("/", rutas);
 
-
-
-app.listen(port, () => console.log("Server running in port", port))
+app.listen(port, () => console.log("Server running in port", port));
